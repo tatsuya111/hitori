02aa9a02b010b6048795a98ff6ea4ded7c6e372a
NaClaq <s6783d3fd7xgtzm@iwama-no-MacBook-Air.local>
2015-01-16 16:28:42 +0900

    追加

---
+++ Bodies/Bodies.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+import jp.ac.kyoto_su.cse.pl.Example as example
+
+if __name__ == '__main__': sys.exit(example.main())
---
+++ Bodies/setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 
+
+from distutils.core import setup
+
+setup(
+	name='Bodies',
+	version='1.0.2',
+	description='Bodies written by Python and PyOpenGL',
+	license='The BSD 2-Clause License',
+	long_description='ドラゴン・スズメバチ・うさぎ・ペンギン・鬼・赤ちゃんを3次元描画する',
+	platforms='OS X (10.9.1)',
+	packages=['jp'],
+)
